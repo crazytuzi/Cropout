95916cdf790d534a4ded80dfab005304496d401f
crazytuzi <957824770@qq.com>
2023-12-02 11:57:45 +0800

    UIE_Cost_C

---
+++ Script/Game/Game/UI/UI_Elements/UIE_Cost_C.cs
@@ -0,0 +1,40 @@
+using Script.Common;
+using Script.Engine;
+using Script.Game.Blueprint.Interactable.Extras;
+using Script.Game.Environment.Materials.Textures;
+using Script.Game.UI.Materials.Textures;
+using Script.Library;
+
+namespace Script.Game.UI.UI_Elements
+{
+    [IsOverride]
+    public partial class UIE_Cost_C
+    {
+        [IsOverride]
+        public override void PreConstruct(bool IsDesignTime)
+        {
+            C_Cost.SetText(Cost.ToString());
+
+            UTexture2D SoftTexture = null;
+
+            if (Resource == E_ResourceType.None)
+            {
+                SoftTexture = Unreal.LoadObject<_h01_127grey_VT>(this);
+            }
+            else if (Resource == E_ResourceType.Food)
+            {
+                SoftTexture = Unreal.LoadObject<T_Rsrc_Food_DA>(this);
+            }
+            else if (Resource == E_ResourceType.Wood)
+            {
+                SoftTexture = Unreal.LoadObject<T_Rsrc_Wood_DA>(this);
+            }
+            else if (Resource == E_ResourceType.Stone)
+            {
+                SoftTexture = Unreal.LoadObject<T_Rsrc_Stone_DA>(this);
+            }
+
+            Image_17.SetBrushFromTexture(SoftTexture);
+        }
+    }
+}
